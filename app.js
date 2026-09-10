2fa369f6240945d1edfad9e6488414327ecad4c9
Yash Vasani <vasaniyash9646@gmail.com>
2026-09-10 08:49:51 +0530

    add all data on backend

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+let products = [];
+
+app.post("/products", (req, res) => {
+  const product = {
+    id: Date.now(),
+    product: req.body.product,
+    price: req.body.price,
+  };
+
+  products.push(product);
+
+  res.send(product);
+});
+
+app.get("/products", (req, res) => {
+  res.set("Cache-Control", "no-store");
+  res.send(products);
+});
+
+app.put("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const product = products.find((product) => {
+    return product.id === id;
+  });
+
+  product.product = req.body.product;
+  product.price = req.body.price;
+
+  console.log(product);
+  res.send(product);
+});
+
+app.delete("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+  products = products.filter((item) => item.id !== id);
+  console.log(products);
+  res.send("Product deleted successfully");
+});
+
+app.listen(port, () => {
+  console.log(`Example app http://localhost:${port}`);
+});
